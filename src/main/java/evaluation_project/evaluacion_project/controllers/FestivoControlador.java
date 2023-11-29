5bcc946530534a11e47d7b6fe08b130c3151a4e5
Luis Felipe Cadavid <felipe.cadavid@udea.edu.co>
2023-11-29 05:23:36 -0500

    se agrega comando para habiltar cors

--- src/main/java/evaluation_project/evaluacion_project/controllers/FestivoControlador.java
+++ src/main/java/evaluation_project/evaluacion_project/controllers/FestivoControlador.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import evaluation_project.evaluacion_project.services.FestivoServicio;
 
 @RestController
 @RequestMapping("/festivos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FestivoControlador {
 
     @Autowired
